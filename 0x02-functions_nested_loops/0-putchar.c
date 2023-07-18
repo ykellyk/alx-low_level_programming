9dc10f65f46318b153f8a3da1fb5ac7b4a9fd5a2
ykellyk <yk.kelly.kelly@gmail.com>
2023-07-18 11:14:06 -0700

    seventh

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * 
+ * Description:
+ * This program prints the string "_putchar" followed by a new line.
+ *
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
