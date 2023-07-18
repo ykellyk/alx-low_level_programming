bc1f6f403b44445e8c2d48aef89e3509f8234eef
ykellyk <yk.kelly.kelly@gmail.com>
2023-07-18 12:44:38 -0700

    seventh

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,12 @@
 /**
  * _islower - Checks whether a character is lowercase or not
  * @c: The character to be checked
- * 
+ *
  * Return: 1 if the character is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
