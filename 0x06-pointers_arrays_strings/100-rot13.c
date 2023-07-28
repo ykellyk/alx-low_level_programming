2c1ea1b6c04c02cdef22af92327ebd68e9bb2c30
ykellyk <yk.kelly.kelly@gmail.com>
2023-07-27 19:42:09 -0700

    Mood

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @s: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+char *rot13(char *s)
+{
+int i, j;
+char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (i = 0; s[i]; i++)
+{
+for (j = 0; a[j]; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+break;
+}
+}
+}
+
+return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +9,11 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
+char *rot13(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
+
+#endif
