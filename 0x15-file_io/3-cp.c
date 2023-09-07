46ba7cd991755acb0e7204f07979a6955af11abf
ykellyk <yk.kelly.kelly.com>
2023-09-06 19:58:19 -0700

    Bobby

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,85 +1,83 @@
 #include "main.h"
+
 /**
- * not_close - prints error.
- * @fd: value to print.
+ * error_cases - handling errors according to their states
+ * @error_status: status
+ * @file: file
+ * @fd: file descriptor
  */
 
-void not_close(int fd)
+void error_cases(int error_status, char *file, int fd)
 {
-	dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
-	exit(100);
+	if (error_status == 97)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(error_status);
+	}
+	else if (error_status == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		exit(error_status);
+	}
+	else if (error_status == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(error_status);
+	}
+	else if (error_status == 100)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(error_status);
+	}
 }
 
-
 /**
- * cp - copy a file
- * @from: file to copy
- * @to: file to copy in
+ * main - copies the content of a file to another file
+ * @argc: args num
+ * @argv: array
+ * Return: 0
  */
 
-void cp(char *from, char *to)
+int main(int argc, char **argv)
 {
-	int file_r, file_w, r, w;
+	int fd_from = 0, fd_to = 0, close_status = 0, i;
+	ssize_t bytes_read = 1, bytes_written = 0;
+	char *file_from = NULL, *file_to = NULL;
 	char buff[1024];
 
-	file_r = open(from, O_RDONLY);
-	if (file_r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
-		exit(98);
-	}
-	file_w = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_w == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
-		exit(99);
-	}
-	r = read(file_r, buff, 1024);
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
-		exit(98);
-	}
-	while (r != 0)
-	{
-		w = write(file_w, buff, r);
-		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", to);
-			exit(99);
-		}
-		r = read(file_r, buff, 1024);
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
-			exit(98);
-		}
-	}
-	if (close(file_w) == -1)
-	not_close(file_r);
-	if (close(file_r) == -1)
-	not_close(file_w);
-}
-/**
- * main - copy a file into another
- * @argc: size
- * @argv: character.
- *
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-
 	if (argc != 3)
+		error_cases(97, NULL, 0);
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		error_cases(98, file_from, 0);
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
+		close(fd_from);
+		error_cases(99, file_to, 0);
 	}
-	if (argv[1] == NULL)
+
+	for (i = 0; i < 1024; i++)
+		buff[i] = '\0';
+	while (bytes_read)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		bytes_read = read(fd_from, buff, 1024);
+		if (bytes_read == -1)
+			error_cases(98, file_from, 0);
+		bytes_written = write(fd_to, buff, bytes_read);
+		if (bytes_written == -1)
+			error_cases(99, file_to, 0);
 	}
-	cp(argv[1], argv[2]);
+
+	close_status = close(fd_from);
+	if (close_status == -1)
+		error_cases(100, NULL, fd_from);
+	close_status = close(fd_to);
+	if (close_status == -1)
+		error_cases(100, NULL, fd_to);
+
 	return (0);
 }
