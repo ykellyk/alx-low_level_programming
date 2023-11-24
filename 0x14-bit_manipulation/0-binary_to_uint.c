e297a9a58c784eeaba9796766ff94528915e052c
ykelly <yk.kelly.kelly@gmail.com>
2023-11-24 10:20:13 -0800

    Fire new

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include "main.h"
+
+int check_valid_string(const char *b)
+{
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b != '1' && *b != '0')
+			return (0);
+		b++;
+	}
+	return (1);
+}
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int str_len = 0, base = 1;
+
+	if (!check_valid_string(b))
+		return (0);
+	while (b[str_len] != '\0')
+		str_len++;
+	while (str_len)
+	{
+		decimal += ((b[str_len - 1] - '0') * base);
+		base *= 2;
+		str_len--;
+	}
+
+	return (decimal);
+}
