441fd12d742ac2896857445ff7d02bd39260a661
ykelly <yk.kelly.kelly@gmail.com>
2024-01-11 09:00:58 -0800

    Jeep

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,18 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - sum of elements in list
- * @head: list
- * Return: int
+ * sum_dlistint - Sums all the data of a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ *
+ * Return: The sum of all the data.
  */
-
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
-	while (head != NULL)
+
+	while (head)
 	{
 		sum += head->n;
 		head = head->next;
 	}
+
 	return (sum);
 }
