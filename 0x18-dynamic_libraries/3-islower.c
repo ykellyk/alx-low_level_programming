4b2987478028542b4cc45a50a4d5c58d1665d254
ykelly <yk.kelly.kelly@gmail.com>
2024-01-15 06:22:40 -0800

    Gaming

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Checks whether a character is lowercase or not
+ * @c: The character to be checked
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
